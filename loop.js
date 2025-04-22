1c1277d256079e4c75c3730a9a2cdbc8d75a03a1
shohan12727 <aishohan001@gmail.com>
2025-04-22 23:33:04 +0600

    function done

--- loop.js
+++ loop.js
@@ -1,8 +1,25 @@
 const countries = ['Bangladesh', 'India', 'South Africa', 'Srilanka', 'Pakistan', 'Australia'];
 
-for(let i = 0; i< countries.length; i++) {
+for (let i = 0; i < countries.length; i++) {
     const element = countries[i];
     console.log(element);
-    
+}
+
+const fruits = [
+    "Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blackcurrant", "Blueberry", "Boysenberry", "Breadfruit", "Cantaloupe",
+    "Cherimoya", "Cherry", "Chico", "Clementine", "Cloudberry", "Coconut", "Cranberry", "Currant", "Custard Apple", "Date",
+    "Dragonfruit", "Durian", "Elderberry", "Feijoa", "Fig", "Goji Berry", "Gooseberry", "Grape", "Grapefruit", "Guava",
+    "Hackberry", "Honeyberry", "Honeydew", "Huckleberry", "Indian Fig", "Jackfruit", "Jambul", "Jujube", "Juniper Berry", "Kaffir Lime",
+    "Kiwano", "Kiwi", "Kumquat", "Langsat", "Lemon", "Lime", "Longan", "Loquat", "Lychee", "Mandarin",
+    "Mango", "Mangosteen", "Marionberry", "Melon", "Miracle Fruit", "Mulberry", "Nance", "Nectarine", "Olive", "Orange",
+    "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon", "Pineapple", "Pitaya", "Plantain", "Plum", "Pomegranate",
+    "Pomelo", "Prickly Pear", "Quince", "Raisin", "Rambutan", "Raspberry", "Red Banana", "Redcurrant", "Rose Apple", "Salak",
+    "Satsuma", "Soursop", "Star Apple", "Starfruit", "Strawberry", "Sugar Apple", "Surinam Cherry", "Tamarillo", "Tamarind", "Tangerine",
+    "Ugli Fruit", "Velvet Apple", "Water Apple", "Watermelon", "White Currant", "White Sapote", "Wild Strawberry", "Wineberry", "Yuzu", "Ziziphus Fruit"
+];
+
+for (i = 0; i < fruits.length; i++) {
+    const element = fruits[i];
+    console.log(element);
 }
 
---
+++ templeteString.js
@@ -0,0 +1,11 @@
+const nationalAnthem = `amar sonar bangla,  ami tomay valobasi , cirodin tomar akash, tomar batash`;
+console.log(nationalAnthem);
+
+const country = 'Bangladesh';
+const comment =  `I love ${country}.`;
+console.log(comment);
+
+const myComment = `Hey, how are you? I am fine .what do you expect from me ? tell me `;
+console.log(myComment);
+
+
