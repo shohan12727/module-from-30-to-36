de6fe12708d2bbd7fb0a530940305b8afb8de7fd
shohan12727 <aishohan001@gmail.com>
2025-04-22 22:39:08 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,8 @@
+const countries = ['Bangladesh', 'India', 'South Africa', 'Srilanka', 'Pakistan', 'Australia'];
+
+for(let i = 0; i< countries.length; i++) {
+    const element = countries[i];
+    console.log(element);
+    
+}
+
