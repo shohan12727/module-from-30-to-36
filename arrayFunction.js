729320f6fb034571f14d0c31a3712016732ecfef
shohan12727 <aishohan001@gmail.com>
2025-04-23 08:50:29 +0600

    array function done

---
+++ arrayFunction.js
@@ -0,0 +1,16 @@
+function sum (a,b) {
+    return a+b ;
+}
+console.log(sum (55,96));
+//  now i will do this with array function 
+
+const additon = (a,b) => a+b;
+
+console.log(additon(58,956));
+
+const subtraction = (a,b) => {
+    return a-b;
+}
+console.log(subtraction(55,78));
+
+
