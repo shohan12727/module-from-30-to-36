ea3fbec0eeab305a54a51376cb483b98632f9808
shohan12727 <aishohan001@gmail.com>
2025-04-27 13:48:32 +0600

    odule 32 done

--- main_/increment.js
+++ main_/increment.js
@@ -1,11 +1,16 @@
 let age = 20;
-// age = age + 1;
+age = age + 1;
 
-// console.log (age += 1);
-// age ++ 
+console.log (age += 1);
+age ++ 
 
-// console.log(age++);
-// console.log(age);
+console.log(age++);
+console.log(age);
 
 console.log(age=age-1);
 console.log(age -= 1);
+
+
+
+
+
