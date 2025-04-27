3e493c2d84ceb79955dbd46e82d5c589bfeadb93
shohan12727 <aishohan001@gmail.com>
2025-04-27 16:13:13 +0600

    api

---
+++ module_33/app.js
@@ -0,0 +1,18 @@
+const person = {
+    name:"shohan",
+    age:20,
+    friends : ["raje3sh",'pal','gandu'],
+    status : true
+}
+
+// console.log(person.friends[2]);
+
+//json data is similar like js object 
+
+const newPerson = JSON.stringify(person)
+console.log(newPerson);
+// console.log(typeof(newPerson));
+const newPerson2 = JSON.parse(newPerson);
+console.log(newPerson2);
+
+
