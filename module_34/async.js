40f293913367b0d9334c5f3dc919e73e5c44e5ad
shohan12727 <aishohan001@gmail.com>
2025-05-01 09:28:11 +0600

    async example

---
+++ module_34/async.js
@@ -0,0 +1,15 @@
+const num1 = 10;
+const num2 = 20;
+
+function sum() {
+    return num1 + num2;
+}
+
+setTimeout(() => {
+    console.log("ami clown");
+},3000)  // sobar sese run hobe
+console.log("ami prothom");
+
+console.log("ami second");
+
+console.log(sum ()) // third e eita execute hobe 
