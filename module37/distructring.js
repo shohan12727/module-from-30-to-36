ad46c73652a5760a8b4ce66326bd575ffb8617d3
shohan12727 <aishohan001@gmail.com>
2025-05-04 16:44:10 +0600

    distructring done

---
+++ module37/distructring.js
@@ -0,0 +1,21 @@
+const element1 = friends[1];
+const element2 = friends[2];
+const element3 = friends[3];
+console.log(element1,element2,element3);
+const friends = ['shohan', 'rajesh', 'pal', 'simanto', 'koishik'];
+
+const [element1, element2, element3] = friends;
+console.log(element1);
+console.log(element2);
+console.log(element3);
+
+const person = {
+    name: 'Shohan',
+    age: 20,
+    university: 'nsu',
+    Friends: ['pal', 'pussy pal']
+}
+
+const { name, age, Friends} = person;
+console.log(Friends[1]);
+
