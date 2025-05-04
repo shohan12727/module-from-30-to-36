89c42fb12da57600600fb592eab818b654e189ff
shohan12727 <aishohan001@gmail.com>
2025-05-04 11:23:57 +0600

    tamplet and  spread operator done recap done

---
+++ module37/tamplet.js
@@ -0,0 +1,41 @@
+// tamplet string 
+const name ="Shohan";
+const finalName = ' islam';
+
+console.log(name+finalName);
+
+console.log(`fuck concotanation..🛴 only tamplet string is real
+    ..damm................
+     Ashraful Islam Shohan`);
+
+  console.log(`i am ${name}`);
+
+//   ------------------------------------------------------------------
+ 
+function sumss () {
+
+}
+
+const sum = (a,b) => a+b;
+console.log(sum (5,5));
+
+// -----------------------------------------------------------------------
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+console.log(numbers);
+console.log(...numbers);
+
+const newNumbers = [numbers,10,56,96]
+console.log(newNumbers);
+
+const newNumbers2 = [...numbers,10,56,96,455];
+console.log(...newNumbers2);
+
+
+
+
+
+
+
+
